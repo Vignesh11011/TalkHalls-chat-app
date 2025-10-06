b2ae7660fbf9cde48e5b8da6a7890df45c064e38
Vignesh11011 <vsai4401@gmail.com>
2025-10-06 22:48:23 +0530

    Old ver

---
+++ public/js/main.js
@@ -0,0 +1,67 @@
+const chatform=document.getElementById('message-input');
+const chatbox=document.getElementById('messages')
+const socket=io();
+const hallName=document.getElementById('currenthall');
+const MemList=document.getElementById('memberList');
+
+//query string extraction
+const {username,hall}=Qs.parse(location.search,{ignoreQueryPrefix:true});
+socket.emit('joinHall',{username,hall});
+document.getElementById('currentUser').innerText=username;
+socket.on('hallMems',({hall,users})=>{
+outputHallname(hall);
+outputMembers(users);
+});
+
+socket.on('Message',message=>{
+   // console.log(message);
+    outputMessage(message);
+    chatbox.scrollTop=chatbox.scrollHeight;
+});
+
+function outputMessage(message){
+    const a= document.createElement('div');
+    a.classList.add('message');
+    if(username==message.username){
+    a.classList.add('sent');}
+    else if(message.username=='HallsManager'){
+        a.classList.add('manager');
+    }
+    else{
+    a.classList.add('received'); 
+    }
+    a.innerHTML=`<span>${message.username}</span><span class="time" >${message.time}</span><br>${message.text}`;
+    document.querySelector('.messages').appendChild(a);
+}
+// message submit
+chatform.addEventListener('submit',(e)=>{e.preventDefault();
+    const msg=e.target.elements.messageBox.value;
+    socket.emit('chatMessage',msg);
+    e.target.elements.messageBox.value='';
+    e.target.elements.messageBox.focus();
+
+})
+
+
+//Room details 
+
+function outputHallname(hall){
+    hallName.innerText=hall;
+
+}
+function outputMembers(users){
+    MemList.innerHTML=`${users.map(user=>`<li>${user.username}</li>`).join('')}`;
+}
+
+
+
+//for changing hall
+
+const a=document.getElementById('joinhallBtn');
+
+a.addEventListener('click',(event)=>{
+    event.preventDefault();
+    const newHall= document.getElementById('hallSelect').value;
+    window.location.href='chat.html'+`?username=${username}&hall=${newHall}`;
+
+});
---
+++ server.js
@@ -0,0 +1,49 @@
+const express=require('express');
+const path= require('path');
+const http=require('http');
+const socketio=require('socket.io');
+const formatMsg=require('./utils/messages');
+const {userJoin,currentUser,userLeave,getRoomlist}=require('./utils/users');
+//create server
+const app=express();
+const server=http.createServer(app);
+const io =socketio(server);
+
+const mngrName='HallsManager';
+
+// static folders
+app.use(express.static(path.join(__dirname,'public')));
+
+//runs when client connects
+
+io.on('connection',socket=>{
+
+    socket.on('joinHall',({username,hall})=>{
+      const user=userJoin(socket.id,username,hall);
+      socket.join(user.hall);
+
+      socket.emit('Message',formatMsg(mngrName,'Welcome to TalkHalls!. Remember members, once you reload your page all previous chats will be not visible to you.Enjoy your meeting!'));
+      //when people join or leave
+      socket.broadcast.to(user.hall).emit('Message',formatMsg(mngrName,`${user.username} has entered the hall`));
+
+      io.to(user.hall).emit('hallMems',{hall:user.hall,users:getRoomlist(user.hall)});
+
+    });
+
+      socket.on('chatMessage',(msg)=>{
+        const user=currentUser(socket.id);
+        io.to(user.hall).emit('Message',formatMsg(user.username,msg))
+      });
+      
+    socket.on('disconnect',()=>{
+      const user=userLeave(socket.id);
+      if(user){
+        io.to(user.hall).emit('Message',formatMsg(mngrName,`${user.username} has left the hall`));
+        io.to(user.hall).emit('hallMems',{hall:user.hall,users:getRoomlist(user.hall)});
+      }
+    });
+
+    });
+
+const PORT= 3000 || process.env.PORT;
+server.listen(PORT,()=>console.log(`Server running on PORT ${PORT}`));
